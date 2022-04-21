2838698cc44f6eec53f098cc71f7b895e46c5b43
Skubenyk <skubenikl@gmail.com>
2022-04-21 12:06:13 +0300

    foundations js

---
+++ index.js
@@ -0,0 +1,88 @@
+//* Правильні імена
+
+let firsName = '1-Yevhenii',
+    lastName = ' Skubenyk';
+
+alert(firsName + lastName);
+
+let firsNameUser = '2-Yevhenii',
+    lastNameUser = ' Skubenyk';
+
+alert(firsNameUser + lastNameUser);
+
+let givenName = '3-Yevhenii',
+    surname = ' Skubenyk';
+
+alert(givenName  + surname);
+
+
+let forename = '4-Yevhenii',
+    nickname = ' Skubenyk';
+
+alert(forename + nickname);
+
+
+
+//* Не правильні імена
+
+/*let 1firsName = 'Yevhenii',
+    2lastName = ' Skubenyk';
+
+let FirstName = 'Yevhenii',
+    LastName = ' Skubenyk';
+
+let FIRST_NAME = 'Yevhenii',
+    LAST_NAME = ' Skubenyk';
+
+let first-name = 'Yevhenii',
+    last-name = ' Skubenyk';*/
+
+// let firsName/ = 'Yevhenii',
+    // lastName/ = ' Skubenyk';
+
+
+//* Привіт Аня!
+let userName = 'Aня';
+prompt('Як тебе звати?');
+
+alert(`Привіт ${userName}!Приємно познайомитись`);
+
+//* Рік народження
+const YEAR_BIRHT = 2022;
+prompt('Який рік твого народження?');
+
+alert(`Тобі ${YEAR_BIRHT - 1986} років!`);
+
+//* Довжина сторони квадрата
+let quadSide = 5;
+prompt('Яка довжина сторони квадрата?');
+
+alert(`Периметр квадрата дорівнює ${quadSide * 4}`);
+
+//* Площа кола
+let circleRadius = 5,
+    pi = 3.14;
+prompt('Який радіус кола?');
+
+alert(`Площа кола дорівнює ${pi * circleRadius **2}`);
+
+//* Відстань між містами
+let distanceS = 300,
+    timeT = 4;
+prompt('Яка відстань від Полтави до Києва?');
+prompt('За який час хочете доїхати до Києва?');
+
+alert(`Швидкість повинна бути ${distanceS / timeT}км/год `);
+
+
+
+
+
+
+
+
+
+
+
+
+
